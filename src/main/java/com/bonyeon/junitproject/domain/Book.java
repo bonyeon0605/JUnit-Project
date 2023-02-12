1104e847b88e1dc935b2aacd4019aa1e13518812
koobonyeon <bonyeun0605@naver.com>
2023-02-12 20:50:05 +0900

    Book 엔티티 생성 완료

--- src/main/java/com/bonyeon/junitproject/domain/Book.java
+++ src/main/java/com/bonyeon/junitproject/domain/Book.java
@@ -1,4 +1,26 @@
 package com.bonyeon.junitproject.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
 }
